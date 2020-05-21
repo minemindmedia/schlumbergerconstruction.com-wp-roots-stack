0fe0f774a656ee766659214df258b6284eb21809
Joshua Busnelli <info@minemindmedia.com>
2020-05-21 12:43:12 -0600

    bpoints footer and services

--- site/web/app/themes/ssc/resources/views/page-services.blade.php
+++ site/web/app/themes/ssc/resources/views/page-services.blade.php
@@ -3,32 +3,36 @@
 @section('content')
     @while(have_posts()) @php the_post() @endphp
     @include('partials.page-header')
+    <div class="mx-8 lg:mx-24 mt-16 lg:mt-32 border-solid border-t-2 border-primary"></div>
         @if( have_rows('services') )
-            @while ( have_rows('services') ) 
-            @php the_row(); @endphp
-                @if( get_row_layout() == 'content_section' )
-                    @php
-                    $title = get_sub_field('section_title');
-                    $content = get_sub_field('section_content');
-                    @endphp
-                    <div class="md:flex">
-                        <div class="w-full md:w-1/3 text-4xl font-normal text-center">
-                            <div class="px-16 py-32">
-                                {{ $title }}
+            
+                @while ( have_rows('services') ) 
+                @php the_row(); @endphp
+                    @if( get_row_layout() == 'content_section' )
+                        @php
+                        $title = get_sub_field('section_title');
+                        $content = get_sub_field('section_content');
+                        @endphp
+                        <div class="md:flex">
+                            <div class="w-full md:w-1/3 text-2xl lg:text-4xl font-normal lg:text-center">
+                                <div class="mx-8 py-16 lg:px-16 lg:py-32">
+                                    {{ $title }}
+                                </div>
+                            </div>
+                            <div class="w-full md:w-2/3 text-xl lg:text-3xl">
+                                <div class="px-8 pb-16 lg:pr-16 lg:py-32">
+                                    {!! $content !!}
+                                </div>
                             </div>
                         </div>
-                        <div class="w-full md:w-2/3 text-3xl">
-                            <div class="pr-16 py-32">
-                                {!! $content !!}
-                            </div>
-                        </div>
-                    </div>
-                @elseif( get_row_layout() == 'image_divider' )
-                    @php $image = get_sub_field('image_divider'); @endphp
-                    <img data-src="{{ $image['sizes']['hero'] }}" alt="" class="lozad w-full">
-                @endif
-            @endwhile
-            @else
+                    @elseif( get_row_layout() == 'image_divider' )
+                        @php $image = get_sub_field('image_divider'); @endphp
+                        <img data-src="{{ $image['sizes']['hero'] }}" alt="" class="lozad w-full">
+                    @endif
+                @endwhile
+                @else
+            
         @endif
+        <div class="mx-8 lg:mx-24 mb-16 lg:mb-32 border-solid border-t-2 border-primary"></div> 
     @endwhile
 @endsection
--- site/web/app/themes/ssc/resources/views/partials/footer.blade.php
+++ site/web/app/themes/ssc/resources/views/partials/footer.blade.php
@@ -1,20 +1,20 @@
-<footer class="bg-primary block p-24">
-    <div class="flex">
-        <div class="w-2/3">
-            <div class="flex">
-                <div class="w-2/3">
-                    <p>Let us know how we can assist in your next project!</p>
+<footer class="bg-primary block p-8 lg:p-24">
+    <div class="block lg:flex">
+        <div class="w-full lg:w-2/3">
+            <div class="block lg:flex">
+                <div class="w-full lg:w-2/3">
+                    <p class="leading-normal text-xl lg:text-3xl">Let us know how we can assist in your next project!</p>
                     @php echo do_shortcode('[contact-form-7 id="67" title="Contact Form"]') @endphp
                 </div>
             </div>
         </div>
-        <div class="w-1/3 contact">
-            <p>
+        <div class="w-full lg:w-1/3 contact">
+            <p class="text-xl lg:text-3xl">
             General Contracting<br>
             Custom Residential + Commercial
             </p>
-            <p>(970) 925-8360</p>
-            <p class="address">
+            <p class="text-xl lg:text-3xl">(970) 925-8360</p>
+            <p class="text-lg lg:text-2xl address">
             309K Aspen Business Center<br>
             Aspen, Colorado 81611
             </p>
