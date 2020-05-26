3ebc8cfe3ea0c46ebb10c82afa95f32fb8d51e69
Joshua Busnelli <info@minemindmedia.com>
2020-05-26 11:58:07 -0600

    people page

--- site/web/app/themes/ssc/resources/views/page-people.blade.php
+++ site/web/app/themes/ssc/resources/views/page-people.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     @while(have_posts()) @php the_post() @endphp
     @include('partials.page-header')
-        @if( have_rows('profile') )
-            @while ( have_rows('profile') ) 
+        @if( have_rows('people') )
+            @while ( have_rows('people') ) 
             @php the_row(); @endphp
                 @if( get_row_layout() == 'content_section_23_-_13' )
                     @php
@@ -57,49 +57,54 @@
             @endwhile
             @else
         @endif
-        @if( have_rows('relationships') )
-            @while( have_rows('relationships') )
+        @if( have_rows('our_team') )
+            @while( have_rows('our_team') )
             @php the_row() @endphp
-                <h3 class="w-full lg:w-3/5 lg:mx-auto mb-12 lg:mb-32  text-center text-2xl lg:text-4xl"><?php the_sub_field('intro'); ?></h3> 
-                <div class="block lg:flex mx-8 lg:mx-32 mb-8 lg:mb-32">
+                <div class="block lg:flex mx-8 lg:mx-64 mb-8 lg:mb-32">
                     <div class="w-full lg:w-1/3">
-                        @if( have_rows('architects') )
-                            @while( have_rows('architects') )
-                            @php
-                            the_row();
-                            $title = get_sub_field('title');
-                            $list = get_sub_field('list');
-                            @endphp
-                                <h3 class="text-lg lg:text-3xl mb-12 mt-12 lg:mt-0 font-medium">{!! $title !!}</h3>
-                                <div class="text-lg lg:text-2xl leading-normal">{!! $list !!}</div>
-                            @endwhile
-                        @endif
+                        <div class="w-1/2 mx-auto text-center lg:text-left">
+                            @if( have_rows('managers') )
+                                @while( have_rows('managers') )
+                                @php
+                                the_row();
+                                $title = get_sub_field('title');
+                                $list = get_sub_field('list');
+                                @endphp
+                                    <h3 class="text-lg lg:text-3xl mb-12 mt-12 lg:mt-0 font-medium">{!! $title !!}</h3>
+                                    <div class="text-lg lg:text-2xl leading-normal">{!! $list !!}</div>
+                                @endwhile
+                            @endif
+                        </div>
                     </div>
                     <div class="w-full lg:w-1/3">
-                        @if( have_rows('interior_designers') )
-                            @while( have_rows('interior_designers') )
-                            @php
-                            the_row();
-                            $title = get_sub_field('title');
-                            $list = get_sub_field('list');
-                            @endphp
-                                <h3 class="text-lg lg:text-3xl mb-12 mt-12 lg:mt-0 font-medium">{!! $title !!}</h3>
-                                <div class="text-lg lg:text-2xl leading-normal">{!! $list !!}</div>
-                            @endwhile
-                        @endif
+                        <div class="w-1/2 mx-auto text-center lg:text-left">
+                            @if( have_rows('supervisors') )
+                                @while( have_rows('supervisors') )
+                                @php
+                                the_row();
+                                $title = get_sub_field('title');
+                                $list = get_sub_field('list');
+                                @endphp
+                                    <h3 class="text-lg lg:text-3xl mb-12 mt-12 lg:mt-0 font-medium">{!! $title !!}</h3>
+                                    <div class="text-lg lg:text-2xl leading-normal">{!! $list !!}</div>
+                                @endwhile
+                            @endif
+                        </div>
                     </div>
                     <div class="w-full lg:w-1/3">
-                        @if( have_rows('landscape_architects') )
-                            @while( have_rows('landscape_architects') )
-                            @php
-                            the_row();
-                            $title = get_sub_field('title');
-                            $list = get_sub_field('list');
-                            @endphp
-                                <h3 class="text-lg lg:text-3xl mb-12 mt-12 lg:mt-0 font-medium">{!! $title !!}</h3>
-                                <div class="text-lg lg:text-2xl leading-normal">{!! $list !!}</div>
-                            @endwhile
-                        @endif
+                        <div class="w-1/2 mx-auto text-center lg:text-left">
+                            @if( have_rows('team') )
+                                @while( have_rows('team') )
+                                @php
+                                the_row();
+                                $title = get_sub_field('title');
+                                $list = get_sub_field('list');
+                                @endphp
+                                    <h3 class="text-lg lg:text-3xl mb-12 mt-12 lg:mt-0 font-medium">{!! $title !!}</h3>
+                                    <div class="text-lg lg:text-2xl leading-normal">{!! $list !!}</div>
+                                @endwhile
+                            @endif
+                        </div>
                     </div>
                 </div>	
             @endwhile
