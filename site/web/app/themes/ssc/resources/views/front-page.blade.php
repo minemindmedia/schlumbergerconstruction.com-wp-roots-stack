4cedd6423356e7af4d120cc2f0915f937a9c74f7
Joshua Busnelli <info@minemindmedia.com>
2020-06-04 16:19:53 -0600

    fix

--- site/web/app/themes/ssc/resources/views/front-page.blade.php
+++ site/web/app/themes/ssc/resources/views/front-page.blade.php
@@ -21,7 +21,6 @@
                     <div class="absolute top-0 right-0 bottom-0 left-1/2 bg-secondary opacity-20 z-50 m-4 p-4">
                         <div class="absolute bottom-0 right-0 text-right text-white pr-8 pb-8">
                             <p class="m-0 text-white">{{ $f->title }}</p>
-                            <p class="m-0 text-white text-2.5xl">{{ ArchivePortfolio::portfolioDetails(get_the_ID())->location }}</p>
                         </div>
                     </div>     
                     {!! $f->thumbnail !!}
