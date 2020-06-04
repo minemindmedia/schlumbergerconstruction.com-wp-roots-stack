f29772f4e03b6ca3d51a8014909ce524e6b20de4
Joshua Busnelli <info@minemindmedia.com>
2020-06-04 15:04:17 -0600

    cpt update

--- site/web/app/themes/ssc/app/cpts.php
+++ site/web/app/themes/ssc/app/cpts.php
@@ -6,6 +6,13 @@ add_action( 'init', function() {
 		# Add the post type to the site's main RSS feed:
 		'show_in_feed' => true,
 
+		'supports' => [
+			'title',
+			'thumbnail' ,
+			'custom-fields' ,
+			'page-attributes',
+		  ],
+
 		# Show all posts on the post type archive:
 		'archive' => [
 			'nopaging' => true,
@@ -25,7 +32,14 @@ add_action( 'init', function() {
 				'meta_key'    => 'published_date',
 				'date_format' => 'd/m/Y'
 			],
-        ],
+		],
+		
+		'site_sortables' => [
+			'menu_order' => array(
+			  'post_field' => 'menu_order',
+			  'default'  => 'ASC',
+			),
+		  ]
         
 	], [
 
@@ -36,5 +50,32 @@ add_action( 'init', function() {
 
 	] );
 
-	
 } );
+
+add_action( 'pre_get_posts', function($query) {
+
+	if ( !is_admin() && $query->is_main_query() && is_post_type_archive('portfolio') ) {
+	  $query->set( 'posts_per_page', 100 );
+	  $query->set( 'orderby', 'menu_order' );
+	  $query->set( 'order', 'ASC' );
+	  $query->set(
+		'meta_query', array(
+		  'meta_query' => array(
+			'relation' => 'OR',
+			array(
+			  'key' => 'private_listing',
+			  'compare' => 'NOT EXISTS'
+			),
+			array(
+			  'key'     => 'private_listing',
+			  'compare' => '!=',
+			  'value'   => 1,
+			),
+		  )
+		)
+	  );
+	} elseif ( is_admin() && $query->is_main_query() && is_post_type_archive('portfolio') ) {
+	  $query->set( 'orderby', 'menu_order' );
+	  $query->set( 'order', 'ASC' );
+	}
+  } );
