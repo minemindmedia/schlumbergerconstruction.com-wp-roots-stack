00b6fe7aed26bc39d309dc91f9b424a6bf61b0a1
Joshua Busnelli <info@minemindmedia.com>
2020-06-08 14:59:26 -0600

    misc breakpoints

--- site/web/app/themes/ssc/resources/views/page-misc.blade.php
+++ site/web/app/themes/ssc/resources/views/page-misc.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 @include('partials.page-header')
     
-    <div class="flex flex-wrap flex-row my-32 mx-16">
+    <div class="flex flex-wrap flex-row my-4 lg:my-32 mx-4 lg:mx-16">
         @foreach($misc as $m)
         @include('partials.content-misc', $m)
         @endforeach
--- site/web/app/themes/ssc/resources/views/partials/content-misc.blade.php
+++ site/web/app/themes/ssc/resources/views/partials/content-misc.blade.php
@@ -3,8 +3,8 @@
         <img src="{{ $photo }}" class="w-full" alt="">
         <div class="overlay absolute top-0 right-0 bottom-0 left-1/2 bg-secondary opacity-20 z-50 m-4 p-4">
             <div class="absolute bottom-0 right-0 text-right text-white pr-8 pb-8">
-                <p class="m-0 text-white text-2.5xl">{{ $name }}</p>
-                <p class="m-0 text-white text-2.5xl">{{ $location }}</p>
+                <p class="m-0 text-white text-lg lg:text-2.5xl">{{ $name }}</p>
+                <p class="m-0 text-white text-lg lg:text-2.5xl">{{ $location }}</p>
                 <img alt="Down Arrow" src="@asset('images/arrow-next.svg')" class="w-12 mt-8 float-right" />
             </div>
         </div>     
