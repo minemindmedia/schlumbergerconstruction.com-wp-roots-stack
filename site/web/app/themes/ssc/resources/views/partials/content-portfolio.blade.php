3a65d2294cc213b8826123e2c091a45b6dd97da4
Joshua Busnelli <info@minemindmedia.com>
2020-06-25 15:01:53 -0600

    subtitle

--- site/web/app/themes/ssc/resources/views/partials/content-portfolio.blade.php
+++ site/web/app/themes/ssc/resources/views/partials/content-portfolio.blade.php
@@ -5,6 +5,7 @@
                 <div class="absolute bottom-0 right-0 text-right text-white pr-8 pb-12">
                     <h6 class="m-0 text-white">{{ get_the_title() }}</h6>
                     <h6 class="m-0 text-white">{{ ArchivePortfolio::portfolioDetails(get_the_ID())->location }}</h6>
+                    @if( get_field('miscellaneous_subtitle') )<h6 class="m-0 text-white">@php the_field( "miscellaneous_subtitle" ) @endphp</h6>@endif
                     <img alt="Down Arrow" src="@asset('images/arrow-next.svg')" class="w-12 mt-8 float-right" />
                 </div>
             </div>     
